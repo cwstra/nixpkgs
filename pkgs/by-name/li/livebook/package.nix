81ec8fe0b112adb3eb2b73ea46048a04b976b596
cwstra <cwstra@gmail.com>
2025-03-08 23:15:10 +0000

    Update livebook

--- pkgs/by-name/li/livebook/package.nix
+++ pkgs/by-name/li/livebook/package.nix
@@ -11,7 +11,7 @@
 }:
 beamPackages.mixRelease rec {
   pname = "livebook";
-  version = "0.14.7";
+  version = "0.15.2";
 
   inherit elixir;
 
@@ -23,13 +23,13 @@ beamPackages.mixRelease rec {
     owner = "livebook-dev";
     repo = "livebook";
     tag = "v${version}";
-    hash = "sha256-8/u/1I3pSfxyF35ZnqClR7FXPL2rhBDdf4w/KUGjVVs=";
+    hash = "sha256-28ToaXa9MLr0WoBDNOtJMs0e4xBjImvRanKl5knI4yw=";
   };
 
   mixFodDeps = beamPackages.fetchMixDeps {
     pname = "mix-deps-${pname}";
     inherit src version;
-    hash = "sha256-FrkM82LO7GIFpKQfhlEUrAuKu33BzPBs6OrWW4C6pI0=";
+    hash = "sha256-srkYi1fH+LoOMwrRaf92/SXCUKnQFUwedg/GbdIbD30=";
   };
 
   postInstall = ''
